a0c9a0f936815e46f7ab1229102f3809cd81bb56
Archi903 <artpele@yandex.ru>
2023-02-02 19:01:08 +0300

    Initial commit

---
+++ src/main/java/me/pele/cooktime/CookTimeApplication.java
@@ -0,0 +1,13 @@
+package me.pele.cooktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/pele/cooktime/CookTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pele.cooktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
